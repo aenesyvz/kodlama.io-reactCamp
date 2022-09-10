bd2ef6bfdef2e924fe421039667bf7e9cdf489fc
aenesyvz <20060403@stu.omu.edu.tr>
2022-09-10 17:05:45 +0300

    first commit

---
+++ camp-project/src/App.js
@@ -0,0 +1,20 @@
+import 'semantic-ui-css/semantic.min.css'
+import { Container } from 'semantic-ui-react';
+import './App.css';
+import Dashboard from './layouts/Dashboard.jsx';
+import Navi from './layouts/Navi';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navi/>
+      <Container className="main">
+        <Dashboard/>
+      </Container>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ camp-project/src/layouts/Navi.jsx
@@ -0,0 +1,40 @@
+import React ,{useState}from 'react'
+import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { Container, Menu } from 'semantic-ui-react'
+import CartSummary from './CartSummary'
+import SignedIn from './SignedIn'
+import SignedOut from './SignedOut'
+
+
+export default function Navi() {
+    const {cartItems} = useSelector(state => state.cart)
+
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const history = useHistory()
+    
+    function handleSignOut() {
+        setIsAuthenticated(false)
+        history.push("/");
+    }
+
+    function handleSignIn() {
+        setIsAuthenticated(true)
+    }
+    return (
+        <div>
+            <Menu inverted fixed="top">
+                <Container>
+                <Menu.Item name='home'  />
+                <Menu.Item name='messages'/>
+
+                <Menu.Menu position='right'>
+                    {cartItems.length>0&&<CartSummary/>}
+                    {isAuthenticated ? <SignedIn signOut={handleSignOut}/>:<SignedOut signIn={handleSignIn}/>}
+                </Menu.Menu>
+                </Container>
+                
+            </Menu>
+        </div>
+    )
+}
